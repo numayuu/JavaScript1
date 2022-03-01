1e4933f9f38057ad50da9e49853cabde9b59fea6
numayuu <n.you.01222@gmail.com>
2022-03-01 23:25:45 +0900

    add

--- app.js
+++ app.js
@@ -8,3 +8,31 @@ function addString(strA){
   let addStr = "Hello " + strA;
   return addStr;
 }
+
+
+def divisor_sum(num, limit)
+  (1..limit).select{ |i| num % i == 0 }.sum
+end
+
+puts divisor_sum(1234567890, 30000000)
+
+def divisors(num)
+  result = []
+  (1..num).each do |i|
+    if num % i == 0
+      result << i
+    end
+  end
+  result
+end
+
+def divisors(num)
+  (1..num).select{ |i| num % i == 0 }
+end
+
+def divisor_sum(num, limit)
+  (1..limit).select{ |i| num % i == 0 }.sum
+end
+
+puts divisor_sum(1234567890, 30000000)
+
